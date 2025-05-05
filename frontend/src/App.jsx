e84a28164fb0e3c959a3dbc65d845ae185e6ddf3
) <pratyushparida36@gmail.com>
2025-05-06 03:28:03 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
+
+function App() {
+  const isOwner = useLocation().pathname.includes("owner");
+
+  return (
+    <>
+
+      {!isOwner && <Navbar/>}
+    </>
+  )
+}
+
+export default App
